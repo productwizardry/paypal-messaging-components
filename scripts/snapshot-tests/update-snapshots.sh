fe71bb211e9ed249ae45178898c920baca8821e7
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-10-27 00:13:25 -0400

    Update script

--- scripts/snapshot-tests/update-snapshots.sh
+++ scripts/snapshot-tests/update-snapshots.sh
@@ -12,15 +12,16 @@ if [[ "$TRAVIS_PULL_REQUEST" = "false" ]] && [[ "$TRAVIS_BRANCH" = "develop" ]];
 
     echo 'UPDATE NEW SNAPSHOTS'
     echo $TEST_PATH_PATTERN
+    npm run test:func:ciupdate
     # npm run test:func:ciupdate -- --testPathPattern $TEST_PATH_PATTERN
-    #
-    # # Uses GNU sed syntax
-    # sed -i -e 's/DIRTY_SNAPSHOTS=1/DIRTY_SNAPSHOTS=0/g' .travis.yml
-    #
-    # git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-    #
-    # git checkout develop
-    # git add .
-    # git commit -m "chore(snapshots): update snapshots [skip ci]"
-    # git push
+
+    # Uses GNU sed syntax
+    sed -i -e 's/DIRTY_SNAPSHOTS=1/DIRTY_SNAPSHOTS=0/g' .travis.yml
+
+    git remote set-url origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
+
+    git checkout develop
+    git add .
+    git commit -m "chore(snapshots): update snapshots [skip ci]"
+    git push
 fi
