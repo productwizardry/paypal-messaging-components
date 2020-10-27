7819859353cd48e155798fd126fa094904b35519
Merlin (they/them) Patterson <mepatterson@paypal.com>
2020-10-27 13:22:38 -0400

    Add style order idea file

---
+++ mutationsStyleOrderIdea.js
@@ -0,0 +1,16 @@
+const object = {
+    styles: {
+        order: ['a', 'c', 'b'],
+        a: [
+            /* some styles */
+        ],
+        b: [
+            /* some styles */
+        ],
+        c: [
+            /* some styles */
+        ]
+    }
+};
+
+export default object;
